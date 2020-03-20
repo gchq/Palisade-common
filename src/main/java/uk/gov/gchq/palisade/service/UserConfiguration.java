f9ff9415c1249be0d82a0642d02e228c638059ae
ac74475 <ac74475@users.noreply.github.com>
2020-03-20 15:14:23 +0000

    PAL-394 updated the UserConfiguration interfacee to getUsers rather than getCacheWarmerFactory

--- src/main/java/uk/gov/gchq/palisade/service/UserConfiguration.java
+++ src/main/java/uk/gov/gchq/palisade/service/UserConfiguration.java
@@ -20,6 +20,6 @@ import java.util.List;
 
 public interface UserConfiguration {
 
-    List<? extends CacheWarmerFactory> getCacheWarmerFactory();
+    List<? extends CacheWarmerFactory> getUsers();
 
 }
