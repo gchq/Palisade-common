de9b95c8b567934a163949a592b7dd83bdc0c45b
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-05-05 16:23:20 +0100

    PAL 390 updated switch statement to use new enum values

--- src/main/java/uk/gov/gchq/palisade/util/ResourceBuilder.java
+++ src/main/java/uk/gov/gchq/palisade/util/ResourceBuilder.java
@@ -111,10 +111,10 @@ public class ResourceBuilder {
 
         // This should be assigning the attributes map to the returned object, once resources support attribute maps
 
-        switch (Scheme.valueOf(normal.getScheme())) {
+        switch (Scheme.valueOf(normal.getScheme().toUpperCase())) {
             // Both file:/ and hdfs:/ schema produce filesystem-like structures
-            case file:
-            case hdfs:
+            case FILE:
+            case HDFS:
                 return filesystemSchema(normal);
             default:
                 throw new IllegalArgumentException("No such implementation for uri scheme " + normal.getScheme());
