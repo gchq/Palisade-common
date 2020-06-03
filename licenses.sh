00bca79d3cb51eb309f3fc0a66e2f02182ed06b0
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-06-03 09:58:01 +0100

    Pal 641 updated script, array is sorted, prints values to output file

--- licenses.sh
+++ licenses.sh
@@ -7,19 +7,55 @@ if [ -f $FILE ]; then
 
   dependencyCount=$(xmllint --xpath 'count(//licenseSummary/dependencies/dependency)' "$FILE")
   declare -a dependency_array=()
+  int=0
 
-  for (( i = 1; i <= dependencyCount; i++ )); do
-    val=""
-    val+=$(xmllint --xpath '//licenseSummary/dependencies/dependency['$i']/licenses/license/url' $FILE | awk -F">" '{print $2}' | awk -F"<" '{print $1}')"_"
-    val+=$(xmllint --xpath '//licenseSummary/dependencies/dependency['$i']/groupId' $FILE | awk -F">" '{print $2}' | awk -F"<" '{print $1}')":"
-    val+=$(xmllint --xpath '//licenseSummary/dependencies/dependency['$i']/artifactId' $FILE | awk -F">" '{print $2}' | awk -F"<" '{print $1}')":"
-    val+=$(xmllint --xpath '//licenseSummary/dependencies/dependency['$i']/version' $FILE | awk -F">" '{print $2}' | awk -F"<" '{print $1}')
-    dependency_array[$i]="$val"
+  for (( a = 1; a <= dependencyCount; a++ )); do
+    licenseCount=$(xmllint --xpath 'count(//licenseSummary/dependencies/dependency['$a']/licenses/license)' "$FILE")
+    if [ "$licenseCount" == 1 ]; then
+      val=""
+      val+=$(xmllint --xpath '//licenseSummary/dependencies/dependency['$a']/licenses/license/url' $FILE | awk -F">" '{print $2}' | awk -F"<" '{print $1}')"_"
+      val+=$(xmllint --xpath '//licenseSummary/dependencies/dependency['$a']/groupId' $FILE | awk -F">" '{print $2}' | awk -F"<" '{print $1}')":"
+      val+=$(xmllint --xpath '//licenseSummary/dependencies/dependency['$a']/artifactId' $FILE | awk -F">" '{print $2}' | awk -F"<" '{print $1}')":"
+      val+=$(xmllint --xpath '//licenseSummary/dependencies/dependency['$a']/version' $FILE | awk -F">" '{print $2}' | awk -F"<" '{print $1}')
+      if [[ "${val}" == http* ]]; then
+          dependency_array[$int]="$val"
+          int=$((int+1))
+      fi
+    else
+      for (( b = 1; b <= licenseCount; b++ )); do
+        val=""
+        val+=$(xmllint --xpath '//licenseSummary/dependencies/dependency['$a']/licenses/license['$b']/url' $FILE | awk -F">" '{print $2}' | awk -F"<" '{print $1}')"_"
+        val+=$(xmllint --xpath '//licenseSummary/dependencies/dependency['$a']/groupId' $FILE | awk -F">" '{print $2}' | awk -F"<" '{print $1}')":"
+        val+=$(xmllint --xpath '//licenseSummary/dependencies/dependency['$a']/artifactId' $FILE | awk -F">" '{print $2}' | awk -F"<" '{print $1}')":"
+        val+=$(xmllint --xpath '//licenseSummary/dependencies/dependency['$a']/version' $FILE | awk -F">" '{print $2}' | awk -F"<" '{print $1}')
+        if [[ "${val}" == http* ]]; then
+          dependency_array[$int]="$val"
+          int=$((int+1))
+        fi
+      done
+    fi
   done
 
-  n="${#dependency_array[@]}"
-  for (( i = 1; i <= n; i++ )); do
-    echo "${dependency_array[$i]}"
+  sorted_array=($(echo "${dependency_array[@]}" | tr ' ' '\n' | sort))
+  n="${#sorted_array[@]}"
+  curLicense=""
+
+  for (( i = 0; i < n; i++ )); do
+      var1=$(echo "${sorted_array[$i]}" | cut -f1 -d_)
+      var2=$(echo "${sorted_array[$i]}" | cut -f2 -d_)
+      if [ $i == 0 ]; then
+        curLicense="$var1"
+        printf '%s\n\n' "$var1" >> output.txt
+        printf '%s\n' "$var2" >> output.txt
+      else
+        if [ "$var1" == "$curLicense" ]; then
+          printf '%s\n' "$var2" >> output.txt
+        else
+          curLicense="$var1"
+          printf '\n\n%s\n\n' "$var1" >> output.txt
+          printf '%s\n' "$var2" >> output.txt
+        fi
+      fi
   done
 
 else
