d22c068d5428545bc0266960f361cdf3cb5fc88d
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-10-27 19:02:32 +0000

    PAL-412, removed policy object and updated prePopulation and IsTextResourceRule to use LeafResources over Resources

--- src/test/java/uk/gov/gchq/palisade/policy/IsTextResourceRule.java
+++ src/test/java/uk/gov/gchq/palisade/policy/IsTextResourceRule.java
@@ -19,18 +19,15 @@ package uk.gov.gchq.palisade.policy;
 import uk.gov.gchq.palisade.Context;
 import uk.gov.gchq.palisade.User;
 import uk.gov.gchq.palisade.resource.LeafResource;
-import uk.gov.gchq.palisade.resource.Resource;
 import uk.gov.gchq.palisade.rule.Rule;
 
 import java.io.Serializable;
 
-public class IsTextResourceRule implements Serializable, Rule<Resource> {
+public class IsTextResourceRule implements Serializable, Rule<LeafResource> {
     @Override
-    public Resource apply(final Resource record, final User user, final Context context) {
-        if (record instanceof LeafResource) {
-            if (((LeafResource) record).getSerialisedFormat().equalsIgnoreCase("txt")) {
-                return record;
-            }
+    public LeafResource apply(final LeafResource record, final User user, final Context context) {
+        if (record.getSerialisedFormat().equalsIgnoreCase("txt")) {
+            return record;
         }
         return null;
     }
