5e2efd1751ec720fe01c3f9fadc5b0f427be5b86
m78233 <61839550+m78233@users.noreply.github.com>
2021-03-02 16:33:28 +0000

    PAL-974-Passthrough-rule-for-data-service: clean-up
    Signed-off-by: m78233 <61839550+m78233@users.noreply.github.com>

--- src/main/java/uk/gov/gchq/palisade/rule/Rule.java
+++ src/main/java/uk/gov/gchq/palisade/rule/Rule.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package uk.gov.gchq.palisade.rule;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
