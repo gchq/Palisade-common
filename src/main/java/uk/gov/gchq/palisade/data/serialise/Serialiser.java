ef42391b00fd9e4fbcab2d8fc55a1baf37734064
dev930018 <56113485+dev930018@users.noreply.github.com>
2021-07-27 17:44:19 +0100

    Pal 1086 data reader reactive streams (#102)

--- src/main/java/uk/gov/gchq/palisade/data/serialise/Serialiser.java
+++ src/main/java/uk/gov/gchq/palisade/data/serialise/Serialiser.java
@@ -15,41 +15,123 @@
  */
 package uk.gov.gchq.palisade.data.serialise;
 
-import java.io.IOException;
+import akka.NotUsed;
+import akka.japi.Pair;
+import akka.stream.Attributes;
+import akka.stream.Materializer;
+import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Keep;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import akka.stream.javadsl.StreamConverters;
+import akka.util.ByteString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.Serializable;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
 import java.util.stream.Stream;
 
 /**
- * Serialisers are the part of Palisade that convert from a particular data serialised format to a stream of objects
- * and back again.
- * <b>IMPORTANT:</b> All instances of this interface must be thread safe. That is, they must support multiple threads performing
- * calling either {@link Serialiser#deserialise(InputStream)} or {@link Serialiser#serialise(Stream, OutputStream)} concurrently.
- * The easiest and recommended way to do this is to make the {@code Serialiser} instance stateless; don't store anything related to
- * a particular de/serialisation operation in class member fields.
+ * Serialisers are the part of Palisade that convert from a particular data serialised format to a stream of objects and back again.
+ * The interface offers some static methods allowing dyamically creating a {@link Serialiser} instance from a resource type and format.
+ * <p>
+ * Also present are default implementations of the akka methods, which are backed by their Java stdlib alternatives.
+ * These may suffice for some implementations, but others may wish to override these defaults with more performant custom implementations.
  *
- * @param <I> the domain object type
+ * @param <T> the domain object type
  */
-public interface Serialiser<I> extends Serializable {
+public interface Serialiser<T> {
+    Logger LOGGER = LoggerFactory.getLogger(Serialiser.class);
 
     /**
-     * Serialises a {@link Stream} of objects to an {@link OutputStream}. If {@code objects} is {@code null}, then
-     * nothing will be written.
+     * Try to create a serialiser instance from a serialiser class and domainClass name (from {@link Class#getName()})
      *
-     * @param objects the stream of objects to be serialised
-     * @param output  the output stream to write the serialised bytes to
-     * @throws IOException if something fails while writing the object stream
+     * @param serialiserClass the serialiser class to construct an instance of
+     * @param domainClass     the domainClass {@link T} for the {@code Serialiser<T>}
+     * @param <T>             the domainClass of the serialiser (type of objects the serialiser accepts)
+     * @return a new {@link Serialiser} of the given class and domainClass
+     * @throws NoSuchMethodException     if no single-element constructor for the {@code Class<T>} type was found
+     * @throws IllegalAccessException    if the constructor had an inaccessible access modifier (e.g. {@code private})
+     * @throws InvocationTargetException if the constructor threw an exception, where the cause can be found with {@link InvocationTargetException#getTargetException()}
+     * @throws InstantiationException    if the {@link Constructor#newInstance(Object...)} method fails for any other reason,
+     *                                   e.g. the class represents an abstract class or interface that is not instantiable
      */
-    void serialise(final Stream<I> objects, final OutputStream output) throws IOException;
+    // Cannot reasonably type this due to Java's generics and type erasure, suppress cast to Serialiser<T> on class reflection
+    @SuppressWarnings({"unchecked"})
+    static <T> Serialiser<T> create(Class<Serialiser<?>> serialiserClass, Class<T> domainClass) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Constructor<Serialiser<?>> constructor = serialiserClass.getDeclaredConstructor(Class.class);
+        return (Serialiser<T>) constructor.newInstance(domainClass);
+    }
 
     /**
-     * Deserialise an {@link InputStream} into a {@link Stream} of objects.
+     * Try to create a serialiser instance from a serialiser class and domainClass name (from {@link Class#getName()})
      *
-     * @param stream the input stream to deserialise
-     * @return the deserialised object
-     * @throws IOException if the input stream couldn't be read from.
+     * @param serialiserClass the serialiser class to construct an instance of
+     * @param domainClassName the class name of the domainClass {@link T} for the {@code Serialiser<T>}
+     * @param <T>             the domainClass of the serialiser (type of objects the serialiser accepts)
+     * @return {@link Optional#of(Object)} if the serialiser was constructed successfully, {@link Optional#empty()} otherwise and log the error that occurred.
      */
-    Stream<I> deserialise(final InputStream stream) throws IOException;
+    // Suppress unchecked cast for domainClassName to Class<T> for some T
+    // Suppress warnings for dynamic class loading from String domainClassName to Class domainClass
+    @SuppressWarnings({"unchecked", "java:S2658"})
+    static <T> Optional<Serialiser<T>> tryCreate(Class<Serialiser<?>> serialiserClass, String domainClassName) {
+        try {
+            return Optional.of(Serialiser.create(serialiserClass, (Class<T>) Class.forName(domainClassName)));
+        } catch (Exception ex) {
+            LOGGER.warn("Failed to construct serialiser for serialiserClass '{}' and domainClassName '{}'", serialiserClass, domainClassName, ex);
+            return Optional.empty();
+        }
+    }
 
+    /**
+     * Convert a stream of objects into serialised bytes
+     *
+     * @param objects the incoming stream of objects to serialise
+     * @return an outgoing stream of serialised bytes
+     */
+    InputStream serialise(final Stream<T> objects);
+
+    /**
+     * Convert a stream of objects into serialised bytes
+     *
+     * @return an akka {@link Flow} converting objects into their serialised {@link ByteString} counterparts
+     */
+    default Flow<T, ByteString, CompletionStage<NotUsed>> serialiseFlow() {
+        return Flow.fromMaterializer((Materializer mat, Attributes attrs) -> {
+            Flow<Stream<T>, InputStream, NotUsed> fn = Flow.fromFunction(this::serialise);
+            Pair<Stream<T>, Sink<T, NotUsed>> pair = StreamConverters.<T>asJavaStream()
+                    .preMaterialize(mat);
+            Source<ByteString, NotUsed> src = Source.single(pair.first()).viaMat(fn, Keep.right())
+                    .flatMapConcat(is -> StreamConverters.fromInputStream(() -> is));
+            return Flow.fromSinkAndSource(pair.second(), src);
+        });
+    }
+
+    /**
+     * Convert a stream of bytes into deserialised objects
+     *
+     * @param stream the incoming stream of bytes to deserialise
+     * @return an outgoing stream of objects
+     */
+    Stream<T> deserialise(final InputStream stream);
+
+    /**
+     * Convert a stream of bytes into deserialised objects
+     *
+     * @return an akka {@link Flow} converting {@link ByteString}s into their deserialised object counterparts
+     */
+    default Flow<ByteString, T, CompletionStage<NotUsed>> deserialiseFlow() {
+        return Flow.fromMaterializer((Materializer mat, Attributes attrs) -> {
+            Flow<InputStream, Stream<T>, NotUsed> fn = Flow.fromFunction(this::deserialise);
+            Pair<InputStream, Sink<ByteString, NotUsed>> pair = StreamConverters.asInputStream()
+                    .preMaterialize(mat);
+            Source<T, NotUsed> src = Source.single(pair.first()).viaMat(fn, Keep.right())
+                    .flatMapConcat(is -> StreamConverters.fromJavaStream(() -> is));
+            return Flow.fromSinkAndSource(pair.second(), src);
+        });
+    }
 }
